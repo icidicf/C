0cda2ad7a5938f865451d5a98a8dc54f50711bdc
lyp <hzlouyangping@corp.netease.com>
2016-03-08 09:59:35 +0800

    add time example

--- basic/time/time.c
+++ basic/time/time.c
@@ -6,10 +6,14 @@
 int main(void)
 {
 
-    time_t t;
+    time_t t, t2;
+    int x;
     struct tm *local;
-    t = time(0);   //获取日历时间
-    printf("seconds=%d\n",(int)t);
+    t = time(&t2);   //获取日历时间
+    printf("seconds=%d, t2 %d\n",(int)t, (int)t2);
+    x = t;
+    printf("x is %d\n", x);
+
     
     local = localtime(&t);  //将日历时间转化为本地时间，并保存在struct tm结构中
     printf("local hour is :%d\n",local->tm_hour);
